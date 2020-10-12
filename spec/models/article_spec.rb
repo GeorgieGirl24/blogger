5965828838aa01731bed24c33185360a14382efb
George Soderholm <georgesoderholm@gmail.com>
2020-10-12 11:31:54 -0600

    Add test to validate the body and the title are present

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   describe 'validations' do
-    it { should validated_presence_of(:title) }
-    it { should validate_presenc_of(:body) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
   end
-end 
+end
